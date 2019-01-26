6daca69af4e3720a3c48b9a1b593de1b6f75a7c4
Zara <mckeown-z@email.ulster.ac.uk>
2019-01-26 16:50:50 -0500

    Adding link to deployment and fixing link error

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 https://codeburst.io/deploy-react-to-github-pages-to-create-an-amazing-website-42d8b09cd4d
 
+
+Check out: https://znbb.github.io/aboutme/ for a visual view
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ class Projects extends Component {
               <p><strong>Helping Students Find a Mentor</strong></p>
               <p>MEAN - MongoDB, Express.js, Angular.js, Node.js</p>
               <p><a href="https://mentorme.herokuapp.com" target="_blank" rel="noopener noreferrer">Explore App <i className="fa fa-external-link"/></a></p>
-              <p><a href="https://github.com/znbb/mentorme" target="_blank" rel="noopener noreferrer">Repo <i className="fa fa-github" aria-hidden="true"/></a></p>
+              <p><a href="https://github.com/znbb/mentor.me" target="_blank" rel="noopener noreferrer">Repo <i className="fa fa-github" aria-hidden="true"/></a></p>
             </div>
           </div>
         </div>
